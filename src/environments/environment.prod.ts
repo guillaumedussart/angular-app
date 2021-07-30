41e4e6f64ae5bdc8d71eb723c6cd118ea239c2de
guillaume dussart <guillaume@localdev.local>
2021-07-30 11:10:24 +0200

    change prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrlApiCollegue: 'https://c1.cleverapps.io/collegues/',
+  baseUrlApiVote: 'https://c1.cleverapps.io/votes/',
+  baseUrlApiVoteId: 'https://c1.cleverapps.io/votes?collegue_id=',
+  baseUrlApiAvis: 'https://formation-angular-collegues.herokuapp.com/collegues/',
+  baseUrlApiAvisVote: 'https://formation-angular-collegues.herokuapp.com/votes/'
 };
