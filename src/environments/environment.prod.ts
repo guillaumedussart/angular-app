afd091b2a1307ec0fb6f62036df9fe466521ef67
guillaumedussart <dussartguillaume.dev@gmail.com>
2021-08-16 17:25:54 +0200

    add uri in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,11 @@
 export const environment = {
   production: true,
   baseUrlApiCollegue: 'https://c1.cleverapps.io/collegues/',
+  baseUrlApiCollegueSearch: 'https://c1.cleverapps.io/collegues?q=',
   baseUrlApiVote: 'https://c1.cleverapps.io/votes/',
   baseUrlApiVoteId: 'https://c1.cleverapps.io/votes?collegue_id=',
-  baseUrlApiAvis: 'https://formation-angular-collegues.herokuapp.com/collegues/',
-  baseUrlApiAvisVote: 'https://formation-angular-collegues.herokuapp.com/votes/'
+  baseUrlApiAvis:
+    'https://formation-angular-collegues.herokuapp.com/collegues/',
+  baseUrlApiAvisVote:
+    'https://formation-angular-collegues.herokuapp.com/votes/',
 };
